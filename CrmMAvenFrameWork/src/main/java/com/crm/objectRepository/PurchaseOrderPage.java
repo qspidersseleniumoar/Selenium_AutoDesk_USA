6d9dd762ec5d35436e93ccdde7ac47eb635cfbf5
PrathijnaRaoK <PrathijnaRaoK@LAPTOP-J9U26OEB>
2019-04-03 13:58:40 +0530

    added updated purchase order page

--- CrmMAvenFrameWork/src/main/java/com/crm/objectRepository/PurchaseOrderPage.java
+++ CrmMAvenFrameWork/src/main/java/com/crm/objectRepository/PurchaseOrderPage.java
@@ -2,11 +2,33 @@ package com.crm.objectRepository;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
-public class PurchaseOrderPage {
+import com.crm.commonLib.FileLib;
+import com.crm.commonLib.WebDriverCoominLib;
 
+public class PurchaseOrderPage extends WebDriverCoominLib{
 	@FindBy(xpath="//img[@title='Create Purchase Order...']")
 	private WebElement poCreateImg;
+ 
+	@FindBy(linkText="More")
+	WebElement More;
+	
+	@FindBy(name="Purchase Order")
+	WebElement purchaseOrder;
+
+	@FindBy(xpath="//input[@class='txtBox']")
+	WebElement searchPurchaseorder;
+	
+	@FindBy(id="bas_searchfield")
+	WebElement inDropDown;
+	
+	@FindBy(name="submit")
+	WebElement clickSearch;
+	
+	@FindBy(xpath="//td[text()='PO1 ']")
+	WebElement captureText;
+	
 
 	public WebElement getPoCreateImg() {
 		return poCreateImg;
@@ -16,6 +38,29 @@ public class PurchaseOrderPage {
 	{
 		poCreateImg.click();
 	}
-	
-	
+	public void navigateToMore(){
+		mouseMovement(More);
+	 }
+	public void navigateToPurchaseOrder(){
+        mouseMovement(More);
+			waitAndClick(purchaseOrder);
+	}
+	public void search_purchase_Order() throws Throwable{
+		searchPurchaseorder.click();
+		FileLib flib=new FileLib();
+		searchPurchaseorder.sendKeys(flib.getExcelData("SearchPo", 1, 2));
+	}
+	public void cilckDD(){
+		inDropDown.click();
+		select(inDropDown, "PurchaseOrder No");
+	}
+	public void searchBtnClick(){
+		clickSearch.click();
+	}
+	public void verifySearchedPo(){
+		String expValue="PO1";
+		String actValue=captureText.getText();
+		Assert.assertEquals(actValue, expValue);
+	}
 }
+

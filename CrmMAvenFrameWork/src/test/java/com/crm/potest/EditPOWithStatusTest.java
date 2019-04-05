e2cce1494dd7969c4fa5e3f66a33c84344c11645
Nariyan <Nariyan@LAPTOP-74KNFD1J>
2019-04-05 16:10:11 +0530

    Add edit po with status in POtest

---
+++ CrmMAvenFrameWork/src/test/java/com/crm/potest/EditPOWithStatusTest.java
@@ -0,0 +1,75 @@
+package com.crm.potest;
+
+import java.util.Random;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.crm.commonLib.BaseClass;
+import com.crm.commonLib.FileLib;
+import com.crm.objectRepository.CreatePurchaseOrderPage;
+import com.crm.objectRepository.EditPurchaseOrder;
+import com.crm.objectRepository.Home;
+import com.crm.objectRepository.PurchaseOrderPage;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+/**
+ * 
+ * @author Risha
+ *
+ */
+public class EditPOWithStatusTest extends BaseClass{
+		/**
+		 * @throws editPurchaseOrderStatusTest
+		 * 1. Navigate to Purchase Order module
+		 * 2. Click on create Purchase Order button
+		 * 3. Create Purchase Order
+		 * 4. Change status of Purchase Order
+		 * 5. Verify
+		 */
+
+		@Test
+		public void editPurchaseOrderStatusTest() throws Throwable
+		{
+			ExtentTest testLog = extent.startTest("editPurchaseOrderStatusTest");
+			Random r = new Random();
+			int randomNum = r.nextInt(10000);
+			FileLib elib  = new FileLib();
+			String sub=elib.getExcelData("EditPO",7,2);
+			String vendor=elib.getExcelData("EditPO",2,2);
+			String billingAddress=elib.getExcelData("EditPO",3,2);
+			String shippingAddress=elib.getExcelData("EditPO",4,2);
+			String qty=elib.getExcelData("EditPO",5,2);
+			String price=elib.getExcelData("EditPO",6,2);		
+			
+			testLog.log(LogStatus.INFO, "Navigate to Purchase Order");
+			Home hp=PageFactory.initElements(driver, Home.class);
+			hp.navigateToMore();
+			hp.navigateToPurchaseOrder();
+					
+			testLog.log(LogStatus.INFO, "Navigate to Purchase Order Page");
+			PurchaseOrderPage pop=PageFactory.initElements(driver, PurchaseOrderPage.class);
+			pop.navigateToCreatePo();
+				
+			testLog.log(LogStatus.INFO, "Navigate to Create Purchase Order");
+			CreatePurchaseOrderPage cpop=PageFactory.initElements(driver, CreatePurchaseOrderPage.class);
+			cpop.createPo(sub+" "+randomNum, billingAddress, qty, price);
+			
+			testLog.log(LogStatus.PASS, "Change Status of Purchase Order");
+			EditPurchaseOrder epo = PageFactory.initElements(BaseClass.driver, EditPurchaseOrder.class);
+			epo.editSubject();
+			epo.editStatusName();
+			
+			testLog.log(LogStatus.PASS, " Verification of Status in Purchase Order");
+			String actSubjName = epo.getEditStatus().getAttribute("value");
+			boolean status = actSubjName.contains("Approved");
+			Assert.assertEquals(status, true);		
+			epo.savePurchaseOrderWithNewSubject();
+			extent.endTest(testLog);
+		}
+	}
+
+
+

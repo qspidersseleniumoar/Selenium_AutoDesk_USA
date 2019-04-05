7ca2e969e404840e1c4849f6a2ede911fd5bce73
VENKY <VENKY@DESKTOP-ER90K33>
2019-04-05 17:10:01 +0530

    Adding DeletePo testcase

--- CrmMAvenFrameWork/src/test/java/com/crm/potest/DeletePoTest.java
+++ CrmMAvenFrameWork/src/test/java/com/crm/potest/DeletePoTest.java
@@ -14,7 +14,7 @@ import com.relevantcodes.extentreports.LogStatus;
  * @author AMRUTHA
  *
  */
-public class DeletePoTest extends BaseClass {
+public class DeletePoTest extends BaseClass{
 	/**
 	 * @throws Throwable
 	 * 1.Navigate to PurchaseOrder

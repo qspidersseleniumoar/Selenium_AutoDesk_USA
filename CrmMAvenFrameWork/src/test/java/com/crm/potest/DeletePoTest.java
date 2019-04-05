f3947a78f67b726ca88c7103033e712345b2db56
VENKY <VENKY@DESKTOP-ER90K33>
2019-04-05 16:28:33 +0530

    Adding TestScript of DeletePo

---
+++ CrmMAvenFrameWork/src/test/java/com/crm/potest/DeletePoTest.java
@@ -0,0 +1,42 @@
+package com.crm.potest;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.crm.commonLib.BaseClass;
+import com.crm.objectRepository.Home;
+import com.crm.objectRepository.PurchaseOrderPage;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+/**
+ * 
+ * @author AMRUTHA
+ *
+ */
+public class DeletePoTest extends BaseClass {
+	/**
+	 * @throws Throwable
+	 * 1.Navigate to PurchaseOrder
+	 * 2.Search the PO with PurchaseNo.
+	 * 3.Delete the SearchedPo
+	 * 4.Verification for Deleted PO
+	 */
+@Test
+public void deletePurchaseOrderTest() throws Throwable{
+	ExtentTest testStart=extent.startTest("Delete Po");
+	testStart.log(LogStatus.INFO, "Getting objects from Home");
+	Home home=PageFactory.initElements(driver, Home.class);
+	testStart.log(LogStatus.INFO, "Navigating to PurchaseOrder");
+	home.navigateToMore();
+	home.navigateToPurchaseOrder();
+	testStart.log(LogStatus.INFO, "Searching and Deleting");
+	PurchaseOrderPage poPage=PageFactory.initElements(driver, PurchaseOrderPage.class);
+	poPage.search_purchase_Order();
+	poPage.searchBtnClick();
+	testStart.log(LogStatus.INFO,"Verification for DeletePO");
+	poPage.deletebtnInSearchedPo();
+	extent.endTest(testStart);
+	
+}
+}

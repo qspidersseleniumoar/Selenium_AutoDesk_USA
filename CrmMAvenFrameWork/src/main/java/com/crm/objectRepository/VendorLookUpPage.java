f337b26124fdf12ae093c640b20dd089eb76de5d
PrathijnaRaoK <PrathijnaRaoK@LAPTOP-J9U26OEB>
2019-04-05 12:32:16 +0530

    add new page

--- CrmMAvenFrameWork/src/main/java/com/crm/objectRepository/VendorLookUpPage.java
+++ CrmMAvenFrameWork/src/main/java/com/crm/objectRepository/VendorLookUpPage.java
@@ -2,7 +2,11 @@ package com.crm.objectRepository;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
+/**
+ * 
+ * @author PrathijnaRaoK
+ *
+ */
 public class VendorLookUpPage {
 	
 	@FindBy(xpath="//a[text()='ABB']")

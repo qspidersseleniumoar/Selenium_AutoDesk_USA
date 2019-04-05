ebfcbcd0b7d4faaa15e986d8f0cb527ec8f13ce1
PrathijnaRaoK <PrathijnaRaoK@LAPTOP-J9U26OEB>
2019-04-05 12:14:59 +0530

    commit webdriverCommon lib

--- CrmMAvenFrameWork/src/main/java/com/crm/commonLib/WebDriverCoominLib.java
+++ CrmMAvenFrameWork/src/main/java/com/crm/commonLib/WebDriverCoominLib.java
@@ -62,6 +62,10 @@ public class WebDriverCoominLib extends BaseClass{
 		act.moveToElement(wb).perform();
 	}
 	
+	public String getAlertTest(){
+		return driver.switchTo().alert().getText();
+	}
+	
 	public void waitAndClick(WebElement element) {
 		int count =0;
 		while (count<20) {

4a549bb8fb0156b4e70c659fd9454a2018889afe
DELL <DELL@DELL-PC>
2019-05-09 18:44:57 +0530

    Created Project and verified it

---
+++ CrmMAvenFrameWork/src/test/java/com/crm/producttest/ProductTest.java
@@ -0,0 +1,42 @@
+package com.crm.producttest;
+/**
+ * 
+ * @author Sneha
+ *
+ */
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.crm.commonLib.BaseClass;
+import com.crm.objectRepository.Home;
+import com.crm.objectRepository.ProductCreateNew;
+
+public class ProductTest {
+	
+	public class CreateProjectTest extends BaseClass {
+		@Test
+		public void createProductAndVerify() throws Throwable {
+			String prodName = eLib.getExcelData("Sneha", 0, 1);
+			Home hm = PageFactory.initElements(driver, Home.class);
+			ProductCreateNew prodCrt = PageFactory.initElements(driver, ProductCreateNew.class);
+			/*navigate to product*/
+			hm.navigateToProducts();
+			prodCrt.navigatetocreateprdtname();
+			prodCrt.enterprdtname(prodName);
+			prodCrt.savebtn();
+			/*verify Product added or not*/			
+			String expmsg="Product Information";
+			String actmsg=prodCrt.productnamedetail().getText();
+			boolean status=actmsg.contains(expmsg);
+			//Assert.assertTrue(status);
+			SoftAssert sa = new SoftAssert();
+			sa.assertTrue(status, actmsg);
+			sa.assertAll();
+			System.out.println("=====msg verified======");
+			
+		
+		}
+
+}
+}

db6e3372268edd3fdfc0cac00f64728c2777cc64
NIBEDITA <NIBEDITA@MUKESH-PC>
2019-05-10 18:43:43 +0530

    adding my file

--- CrmMAvenFrameWork/src/main/java/com/crm/objectRepository/ProductCreateNew.java
+++ CrmMAvenFrameWork/src/main/java/com/crm/objectRepository/ProductCreateNew.java
@@ -6,44 +6,123 @@ import org.openqa.selenium.support.FindBy;
 import com.crm.commonLib.BaseClass;
 
 public class ProductCreateNew extends BaseClass {
-	 @FindBy(xpath="//img[@alt='Create Product...']")
-	    private WebElement Createbtn;
-	    
-	    @FindBy(name="productname")
-	    private WebElement productnameedt;
-	    
-	    @FindBy(id="productcode")
-	    private WebElement productnumberedt;
-	    
-	    @FindBy(xpath="//img[@alt='Select']")
-	    private WebElement vendornameclick;
-	    
+	@FindBy(xpath = "//img[@alt='Create Product...']")
+	private WebElement Createbtn;
 
-	    @FindBy(xpath="//input[@value='  Save  ']")
-	    private WebElement savebtn;
-	    
-	    @FindBy(xpath="//span[@class='lvtHeaderText']")
-	    private WebElement productnamedetail;
-	    
-	    public void navigatetocreateprdtname() {
-	    	 Createbtn.click();
-	    }
-	    public void enterprdtname(String producname) {
-	    	productnameedt.sendKeys(producname);
-	   }
-	    
-	    public void enterproductnumberedt(String productnumber) {
-	    	productnumberedt.sendKeys(productnumber);
-	    }
-	    public void savebtn() {
-	    	savebtn.click();
-	   }
-	    public WebElement productnamedetail() {
-	    return	productnamedetail;
-	   }
-	    public void vendornameclick() {
-	    	vendornameclick.click();
-			
-	    }
-	   
+	@FindBy(name = "productname")
+	private WebElement productnameedt;
+
+	@FindBy(id = "productcode")
+	private WebElement productnumberedt;
+
+	@FindBy(id = "jscal_field_sales_start_date")
+	private WebElement saleStartEdit;
+
+	@FindBy(name = "productcategory")
+	private WebElement catagorySelct;
+
+	@FindBy(id = "jscal_field_sales_end_date")
+	private WebElement saleEndEdit;
+
+	@FindBy(id = "vendor_part_no")
+	private WebElement vendorPartNoEdit;
+
+	@FindBy(id = "productsheet")
+	private WebElement proSheetNoEdit;
+
+	@FindBy(name = "manufacturer")
+	private WebElement manufactureSelct;
+
+	@FindBy(name = "start_date")
+	private WebElement supportStartDateEdit;
+
+	@FindBy(name = "expiry_date")
+	private WebElement supportEndDateEdit;
+
+	@FindBy(name = "website")
+	private WebElement websiteEdit;
+
+	@FindBy(id = "mfr_part_no")
+	private WebElement partNoEdit;
+
+	@FindBy(id = "serial_no")
+	private WebElement serialNoEdit;
+
+	@FindBy(xpath = "//img[@alt='Select']")
+	private WebElement vendornameclick;
+
+	@FindBy(xpath = "//input[@value='  Save  ']")
+	private WebElement savebtn;
+
+	@FindBy(xpath = "//span[@class='lvtHeaderText']")
+	private WebElement productnamedetail;
+
+	public void navigatetocreateprdtname() {
+		Createbtn.click();
+	}
+
+	public void enterprdtname(String producname) {
+		productnameedt.sendKeys(producname);
+	}
+
+	public void enterproductnumberedt(String productnumber) {
+		productnumberedt.sendKeys(productnumber);
+	}
+
+	public void salesStartDate(String salesStart) {
+		saleStartEdit.sendKeys(salesStart);
+	}
+
+	public void salesEndDate(String salesEnd) {
+		saleEndEdit.sendKeys(salesEnd);
+	}
+
+	public void catagorySelect(String catName) {
+		wlib.select(catagorySelct, catName);
+	}
+
+	public void vendorPartNo(String vPartNo) {
+		vendorPartNoEdit.sendKeys(vPartNo);
+	}
+
+	public void productSheetNo(String pSheetNo) {
+		proSheetNoEdit.sendKeys(pSheetNo);
+	}
+
+	public void manufactureSelect(String mfrName) {
+		wlib.select(manufactureSelct, mfrName);
+	}
+
+	public void supportStartDate(String supportStart) {
+		supportStartDateEdit.sendKeys(supportStart);
+	}
+
+	public void supportEndDate(String supportEnd) {
+		supportEndDateEdit.sendKeys(supportEnd);
+	}
+
+	public void webSiteEdit(String website) {
+		websiteEdit.sendKeys(website);
+	}
+
+	public void mfrPartNo(String mPartNo) {
+		partNoEdit.sendKeys(mPartNo);
+	}
+
+	public void serialNo(String serial) {
+		serialNoEdit.sendKeys(serial);
+	}
+
+	public void savebtn() {
+		savebtn.click();
+	}
+
+	public WebElement productnamedetail() {
+		return productnamedetail;
+	}
+
+	public void vendornameclick() {
+		vendornameclick.click();
+	}
+
 }

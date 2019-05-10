97f71f2bf53c04fe91ec3e038417ace3b0250db6
praveen-sunkara8634 <50350843+praveen-sunkara8634@users.noreply.github.com>
2019-05-10 21:41:37 +0530

    added @Test method to OrganizationTest.java

--- CrmMAvenFrameWork/src/test/java/com/crm/orgnisationtest/OrgnisationTest.java
+++ CrmMAvenFrameWork/src/test/java/com/crm/orgnisationtest/OrgnisationTest.java
@@ -1,7 +1,7 @@
 package com.crm.orgnisationtest;
 
 /**
- * @author Sneha
+ * @author Sneha, Praveen
  */
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
@@ -11,6 +11,7 @@ import com.crm.commonLib.BaseClass;
 import com.crm.commonLib.WebDriverCoominLib;
 import com.crm.objectRepository.CreateNewOrganization;
 import com.crm.objectRepository.Home;
+import com.crm.objectRepository.OrganizationPage;
 
 public class OrgnisationTest extends BaseClass {
 	@Test
@@ -41,4 +42,27 @@ public class OrgnisationTest extends BaseClass {
 		boolean status2 = actul.contains(expected);
 		Assert.assertTrue(status2);
 	}
+	
+	@Test
+	public void deleteAllOrganizations() throws Throwable {
+		/* step1: navigate to Organizations page */
+		Home hp = PageFactory.initElements(driver, Home.class);
+		hp.clickOnOrganizationsTab();
+		OrganizationPage op = PageFactory.initElements(driver, OrganizationPage.class);
+		System.out.println(op.orgTableSize());
+		if (op.orgTableSize() < 1) {
+			op.QuickCreateOrganizationAssignToGroup("organization", "helwet", "support");
+			hp.clickOnOrganizationsTab();
+			op.deletAllOrganizations();
+			hp.clickOnOrganizationsTab();
+		} else {
+			op.deletAllOrganizations();
+			hp.clickOnOrganizationsTab();
+		}
+
+		System.out.println(op.orgTableSize());
+		boolean status = op.orgTableSize() == 0;
+		Assert.assertTrue(status, "delete all organizations test == FAIL");
+	}
+
 }

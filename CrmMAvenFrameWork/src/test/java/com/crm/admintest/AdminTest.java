33ed3c6d0ae4bdcd97d071a910fbaa5c72d820e2
Tarannum <Tarannum@RUBY-VAIO>
2019-05-10 12:57:23 +0530

    updated AdminTest

--- CrmMAvenFrameWork/src/test/java/com/crm/admintest/AdminTest.java
+++ CrmMAvenFrameWork/src/test/java/com/crm/admintest/AdminTest.java
@@ -21,23 +21,30 @@ public class AdminTest extends BaseClass {
 		/*Cursor is at crm setting*/
 		home.MoveToSettingImg(wlib);
 		home.clickOnSettingImg(wlib);
+		
 		/*click on setting link*/
 		Admin admin = PageFactory.initElements(driver, Admin.class);
+		
 		/* click on SharingAndAccess Link*/
 		admin.clickOnSharingAndAccessLnk(wlib);
+		
 		/* Click on add Privilleges Button*/
 		admin.clickOnaddprivillegesBtn(wlib);
 		/* scroll down*/
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,700)");
+		
 		/* fetching data from excel and select an entity accessedBy  */
 		String option1 = eLib.getExcelData("Admin", 4, 3);
 		admin.selectByaccesseddropDown(wlib, option1);
+		
 		/* fetching data from excel and select an entity by permission  */
 		String option2 = eLib.getExcelData("Admin", 3, 4);
 		admin.selectByPermissiondropDown(wlib, option2);
+		
 		/* click on Add Rule button */
 		admin.clickOnAddBtn(wlib);
+		
 		/* Assert validation */
 		String actual_val=admin.getActual_val().getText();
 		boolean status=actual_val.contains("Role");
